b70aef1d281ec4bf920ee2658994381da70ee3cd
Maarij-Aqeel <maarijaqeel3200@gmail.com>
2026-03-05 00:08:56 +0500

    Created home | Login | Signup page

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'screens/login_screen.dart';
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const WaddahApp());
+}
+
+class WaddahApp extends StatelessWidget {
+  const WaddahApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Waddah',
+      debugShowCheckedModeBanner: false,
+      locale: const Locale('ar', 'AE'), // Force Arabic RTL natively
+      supportedLocales: const [
+        Locale('ar', 'AE'),
+      ],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      theme: ThemeData(
+        fontFamily: GoogleFonts.cairo().fontFamily, // Modern arabic typeface
+        useMaterial3: true,
+        primaryColor: const Color(0xFF9000FF),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFF9000FF),
+          primary: const Color(0xFF9000FF),
+          secondary: const Color(0xFF00C853),
+        ),
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}
